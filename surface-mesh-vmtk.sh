a7938706e5a5654a68cc9be33a07f6fe796c5414
anj1 <alireza.n.j@gmail.com>
2016-02-23 10:49:06 +1300

    Updated surface meshing script to work around VMTK's bugs

--- surface-mesh-vmtk.sh
+++ surface-mesh-vmtk.sh
@@ -1,7 +1,22 @@
-vmtkimagereader -spacing 0.1 0.5125628140703586 0.1 -orientation coronal -ifile ~/airways/scans/mean/avg4-mu0.3/signdist_smth.tif --pipe vmtkimagewriter.py -ofile /tmp/image_volume.vti
+spacing="0.1 0.5125628140703586 0.1"
+in_file="../../airways/scans/mean/avg4-mu0.3/signdist_smth.tif"
 
+# original script; would have been great except
+# VMTK is buggy and doesn't handle coordinates
+# property with orientation=coronal.
+#vmtkimagereader -spacing $spacing -orientation coronal -ifile $in_file --pipe vmtkimagewriter.py -ofile /tmp/image_volume.vti
+#vmtkmarchingcubes -l 255.0 -connectivity 1 -ifile /tmp/image_volume.vti -ofile /tmp/model.vtp
+#vmtksurfacereader -ifile /tmp/model.vtp --pipe vmtksurfacewriter -ofile /tmp/model.stl
+
+# updated script
+vmtkimagereader -spacing $spacing -ifile $in_file --pipe vmtkimagewriter.py -ofile /tmp/image_volume.vti
 vmtkmarchingcubes -l 255.0 -connectivity 1 -ifile /tmp/image_volume.vti -ofile /tmp/model.vtp
-
 vmtksurfacereader -ifile /tmp/model.vtp --pipe vmtksurfacewriter -ofile /tmp/model.stl
 
+
 # export mesh to OFF
+meshlabserver -i /tmp/model.stl -o /tmp/model.OFF
+
+# clean up
+rm /tmp/image_volume.vti
+rm /tmp/model.vtp
