9c6b770adcc79a2e0de24ef60d07cd68c34fb77c
anj1 <anej001@aucklanduni.ac.nz>
2016-02-24 10:11:40 +1300

    Working test of new cut-mean-mesh Update order of plane cutting

--- cut-mean-mesh.sh
+++ cut-mean-mesh.sh
@@ -10,13 +10,13 @@ fi
 # save file as OFF file
 meshlabserver -i $1 -o mm-solid-cut0.off
 
-# front, top, bottom, side1, side2, inlet
-~/src/julia-0.4/julia mesh-cut-plane.jl  0.0   1.0   0.0  55.4284  false mm-solid-cut0.off mm-solid-cut1.off
-~/src/julia-0.4/julia mesh-cut-plane.jl  0.0   0.2  -1.0   9.12188 false mm-solid-cut1.off mm-solid-cut2.off
-~/src/julia-0.4/julia mesh-cut-plane.jl  0.0   0.3   1.0  66.5923  false mm-solid-cut2.off mm-solid-cut3.off
-~/src/julia-0.4/julia mesh-cut-plane.jl -1.0   0.0   0.0  25.9     true mm-solid-cut3.off mm-solid-cut4.off
-~/src/julia-0.4/julia mesh-cut-plane.jl  1.0   0.0   0.0  27.1     true mm-solid-cut4.off mm-solid-cut5.off
-~/src/julia-0.4/julia mesh-cut-plane.jl  0.0  -0.15  1.0  71.6495  false mm-solid-cut5.off mm-solid-cut.off
+# front, inlet, top, bottom, side1, side2
+~/src/julia-0.4/julia mesh-cut-plane.jl  0.0  -1.0   0.0  50.0  true mm-solid-cut0.off mm-solid-cut1.off
+~/src/julia-0.4/julia mesh-cut-plane.jl  0.0   0.15  1.0  32.0  true mm-solid-cut1.off mm-solid-cut2.off
+~/src/julia-0.4/julia mesh-cut-plane.jl  0.0  -0.2  -1.0  48.0  true mm-solid-cut2.off mm-solid-cut3.off
+~/src/julia-0.4/julia mesh-cut-plane.jl  0.0  -0.2   1.0  14.0  true mm-solid-cut3.off mm-solid-cut4.off
+~/src/julia-0.4/julia mesh-cut-plane.jl  1.0  -0.0   0.0  25.0  false mm-solid-cut4.off mm-solid-cut5.off
+~/src/julia-0.4/julia mesh-cut-plane.jl  1.0  -0.0   0.0 -25.0  false mm-solid-cut5.off mm-solid-cut.off
 
 # remove temporary files
 rm mm-solid-cut0.off
