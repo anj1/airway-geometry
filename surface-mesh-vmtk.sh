c84055dd78819cd49e567dcc8c773b2fbf4f35a3
anj1 <anej001@aucklanduni.ac.nz>
2016-02-23 19:12:15 +1300

    First working test of signed_distance.ijm

--- surface-mesh-vmtk.sh
+++ surface-mesh-vmtk.sh
@@ -1,25 +1,26 @@
 set -e 
 
-in_file="../../airways/scans/mean/avg4-mu0.3/signdist_smth.tif"
+in_file="signdist_smth.tif"
 
 # original script; would have been great except
 # VMTK is buggy and doesn't handle coordinates
 # property with orientation=coronal.
 #spacing="0.1 0.5125628140703586 0.1"
 #vmtkimagereader -spacing $spacing -orientation coronal -ifile $in_file --pipe vmtkimagewriter.py -ofile /tmp/image_volume.vti
-#vmtkmarchingcubes -l 255.0 -connectivity 1 -ifile /tmp/image_volume.vti -ofile /tmp/model.vtp
+#vmtkmarchingcubes -l 0.0 -connectivity 1 -ifile /tmp/image_volume.vti -ofile /tmp/model.vtp
 #vmtksurfacereader -ifile /tmp/model.vtp --pipe vmtksurfacewriter -ofile /tmp/model.stl
 
 # updated script
 spacing="0.1 0.1 0.5125628140703586"
 vmtkimagereader -spacing $spacing -ifile $in_file --pipe vmtkimagewriter.py -ofile /tmp/image_volume.vti
-vmtkmarchingcubes -l 255.0 -connectivity 1 -ifile /tmp/image_volume.vti -ofile /tmp/model.vtp
+vmtkmarchingcubes -l 0.0 -connectivity 1 -ifile /tmp/image_volume.vti -ofile /tmp/model.vtp
 vmtksurfacereader -ifile /tmp/model.vtp --pipe vmtksurfacewriter -ofile /tmp/model.stl
 
 # export mesh to OFF
 meshlabserver -i /tmp/model.stl -o /tmp/model.off -s remove-dup.xml
 
 # clean up
+rm $in_file
 rm /tmp/image_volume.vti
 rm /tmp/model.vtp
 rm /tmp/model.stl
