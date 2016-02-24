9aa128b4f8e891b264e6f09b1f86bd63a2e25798
anj1 <anej001@aucklanduni.ac.nz>
2016-02-24 18:08:02 +1300

    Got mesh cutting working after coordinate system update

--- cut-mean-mesh.sh
+++ cut-mean-mesh.sh
@@ -11,12 +11,12 @@ fi
 meshlabserver -i $1 -o mm-solid-cut0.off
 
 # front, inlet, top, bottom, side1, side2
-~/src/julia-0.4/julia mesh-cut-plane.jl  0.0  -1.0   0.0  50.0  true mm-solid-cut0.off mm-solid-cut1.off
-~/src/julia-0.4/julia mesh-cut-plane.jl  0.0   0.15  1.0  32.0  true mm-solid-cut1.off mm-solid-cut2.off
-~/src/julia-0.4/julia mesh-cut-plane.jl  0.0  -0.2  -1.0  48.0  true mm-solid-cut2.off mm-solid-cut3.off
-~/src/julia-0.4/julia mesh-cut-plane.jl  0.0  -0.2   1.0  14.0  true mm-solid-cut3.off mm-solid-cut4.off
-~/src/julia-0.4/julia mesh-cut-plane.jl  1.0  -0.0   0.0  25.0  false mm-solid-cut4.off mm-solid-cut5.off
-~/src/julia-0.4/julia mesh-cut-plane.jl  1.0  -0.0   0.0 -25.0  false mm-solid-cut5.off mm-solid-cut.off
+~/src/julia-0.4/julia mesh-cut-plane.jl  0.0  -1.0    0.0  50.0  false mm-solid-cut0.off mm-solid-cut1.off
+~/src/julia-0.4/julia mesh-cut-plane.jl  0.0   0.15   1.0  32.0  false mm-solid-cut1.off mm-solid-cut2.off
+~/src/julia-0.4/julia mesh-cut-plane.jl  0.0  -0.2   -1.0  48.0  false mm-solid-cut2.off mm-solid-cut3.off
+~/src/julia-0.4/julia mesh-cut-plane.jl  0.0  -0.2    1.0  14.0  false mm-solid-cut3.off mm-solid-cut4.off
+~/src/julia-0.4/julia mesh-cut-plane.jl -1.0  -0.001  0.0  28.1  true mm-solid-cut4.off mm-solid-cut5.off
+~/src/julia-0.4/julia mesh-cut-plane.jl  1.0  -0.001  0.0  28.1  true mm-solid-cut5.off mm-solid-cut.off
 
 # remove temporary files
 rm mm-solid-cut0.off
@@ -28,3 +28,4 @@ rm mm-solid-cut5.off
 
 # remove duplicated vertices and save as STL
 meshlabserver -i mm-solid-cut.off -o $2 -s remove-dup.xml
+rm mm-solid-cut.off
