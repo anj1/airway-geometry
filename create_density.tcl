ce088b10e73e40c292e4ea6a50e370d6f6631dc5
anj1 <alireza.n.j@gmail.com>
2016-03-04 18:05:46 +1300

    Fix typo in CAVITY

--- create_density.tcl
+++ create_density.tcl
@@ -11,10 +11,10 @@ ic_geo_new_family CAVITY
 ic_boco_set_part_color CAVITY 
 
 # create new points using base point+delta
-ic_point {} CAVITT pnt.d.1 pnt.1+vector(-10,-90,-10)
-ic_point {} CAVITT pnt.d.2 pnt.2+vector( 10,-90,-10)
-ic_point {} CAVITT pnt.d.3 pnt.3+vector(-10,-90,-10)
-ic_point {} CAVITT pnt.d.4 pnt.4+vector( 10,-90,-10)
+ic_point {} CAVITY pnt.d.1 pnt.1+vector(-10,-90,-10)
+ic_point {} CAVITY pnt.d.2 pnt.2+vector( 10,-90,-10)
+ic_point {} CAVITY pnt.d.3 pnt.3+vector(-10,-90,-10)
+ic_point {} CAVITY pnt.d.4 pnt.4+vector( 10,-90,-10)
 
 # create new density
 ic_geo_create_density density.cavity 0.3 {pnt.1 pnt.2 pnt.3 pnt.4 pnt.d.1 pnt.d.2 pnt.d.3 pnt.d.4} d_width d_ratio
